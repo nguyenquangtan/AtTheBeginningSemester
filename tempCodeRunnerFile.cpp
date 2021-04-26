fc64de2ca88283c060b493e0a72c8a757ab84ef1
Nguyễn Quang Tân <81596255+nguyenquangtan@users.noreply.github.com>
2021-04-26 19:54:33 +0700

    Delete tempCodeRunnerFile.cpp

--- tempCodeRunnerFile.cpp
+++
@@ -1,7 +0,0 @@
-system("cd C:");
-    // mkdir("C:/2020-2021");
-    // system("cd C:/2020-2021");
-    // mkdir("C:/2020-2021/semester 1");
-    // mkdir("C:/2020-2021/semester 2");
-    // mkdir("C:/2020-2021/semester 3");
-    // mkdir("C:/2020-2021/classes");
